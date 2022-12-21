14a721d332433fd5ed60792473ba9eff1843668a
Anton Popov <anton@bbbro.ru>
2022-12-21 14:58:42 +0300

    #3 add main files and user schema

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+const { PORT = 3000 } = process.env;
+const app = express();
+
+mongoose.connect('mongodb://localhost:27017/bitfilmsdb', (err) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log('connected to MongoDB');
+  }
+});
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}`);
+});
---
+++ models/user.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    validate: [validator.isEmail, 'Проверьте правильность написания электронной почты'],
+  },
+  password: {
+    type: String,
+    required: true,
+    select: false,
+  },
+  name: {
+    type: String,
+    minlength: 2,
+    maxlength: 30,
+    required: false,
+  },
+});
+
+module.exports = mongoose.model('user', userSchema);
