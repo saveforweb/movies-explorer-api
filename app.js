21cb342738811aab436fa516aabcbbebb26252fa
Anton Popov <anton@bbbro.ru>
2022-12-21 18:28:44 +0300

    #3 add signin and signup routes, errors

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const { celebrate, Joi, errors } = require('celebrate');
 const bodyParser = require('body-parser');
+const errorsList = require('./errors/index');
+const { errorCodes } = require('./utils/errorCodes');
+
+const { login, createUser } = require('./controllers/users');
 
 const { PORT = 3000 } = process.env;
 const app = express();
@@ -15,6 +20,40 @@ mongoose.connect('mongodb://localhost:27017/bitfilmsdb', (err) => {
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+app.post('/signin', celebrate({
+  body: Joi.object().keys({
+    email: Joi.string().email().required(),
+    password: Joi.string().required(),
+  }),
+}), login);
+
+app.post('/signup', celebrate({
+  body: Joi.object().keys({
+    email: Joi.string().email().required(),
+    password: Joi.string().required(),
+    name: Joi.string().min(2).max(30),
+  }),
+}), createUser);
+
+app.use('*', (req, res, next) => {
+  next(new errorsList.NotFoundError('Страница не найдена.'));
+});
+
+app.use(errors());
+
+app.use((err, req, res, next) => {
+  const { statusCode = errorCodes.internalServerError, message } = err;
+
+  res
+    .status(statusCode)
+    .send({
+      message: statusCode === errorCodes.internalServerError
+        ? 'На сервере произошла ошибка'
+        : message,
+    });
+  next();
+});
 
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
---
+++ controllers/users.js
@@ -0,0 +1,58 @@
+const { NODE_ENV, JWT_SECRET } = process.env;
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const User = require('../models/user');
+const errorsList = require('../errors/index');
+
+const tokenString = NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret';
+
+module.exports.createUser = (req, res, next) => {
+  const {
+    email, password, name,
+  } = req.body;
+
+  bcrypt.hash(password, 10)
+    .then((hash) => User.create({
+      email, password: hash, name,
+    }))
+    .then((document) => {
+      const user = document.toObject();
+      delete user.password;
+      res.send({ data: user });
+    })
+    .catch((err) => {
+      if (err.name === 'ValidationError') {
+        next(new errorsList.BadRequestError('Переданы некорректные данные при создании пользователя.'));
+      } else if (err.code === 11000) {
+        next(new errorsList.ConflictError('Пользователь с таким email зарегистрован.'));
+      } else {
+        next(err);
+      }
+    });
+};
+
+module.exports.login = (req, res, next) => {
+  const { email, password } = req.body;
+  let userId;
+
+  User.findOne({ email }).select('+password')
+    .then((user) => {
+      if (!user) {
+        return next(new errorsList.UnauthorizedError('Неправильные почта или пароль.'));
+      }
+
+      userId = user._id;
+      return bcrypt.compare(password, user.password);
+    })
+    .then((matched) => {
+      if (!matched) {
+        return next(new errorsList.UnauthorizedError('Неправильные почта или пароль.'));
+      }
+
+      const token = jwt.sign({ _id: userId }, tokenString, { expiresIn: '7d' });
+      return res.send({ token });
+    })
+    .catch((err) => {
+      next(err);
+    });
+};
